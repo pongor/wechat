220069d0b26118549c373c5f21a664c31a20a512
tianxugeng <tianxugeng@163.com>
2016-05-06 18:40:56 +0800

    a

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ define('APP_DEBUG',True);
 // 定义应用目录
 define('APP_PATH','./App/');
 
-define('BIND_MODULE','Home');
+// define('BIND_MODULE','Home');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
