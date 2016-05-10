861738a8c11786ca7bdf8c206b136514b3657f5e
pongor <pongor_yuan@yeah.net>
2016-05-10 11:20:03 +0800

    a

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -3,10 +3,11 @@ return array(
 	//'配置项'=>'配置值'
     'LOAD_EXT_CONFIG' => 'db',
 
-//    'DEFAULT_MODULE'  =>  'Home',
-   // 'BIND_MODULE'       => 'Home',
+    'DEFAULT_MODULE'  =>  'Home',
+    //'BIND_MODULE'       => 'Home',
     'URL_MODEL'         => 2,
-    'MULTI_MODULE'      =>  true,
+  //  'MULTI_MODULE'      =>  true,
+    'MODULE_ALLOW_LIST' => array('Home','Backend')
 
 
 );
