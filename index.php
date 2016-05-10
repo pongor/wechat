735ef222d3456763c8a5d150f0ea0661cbbd617e
pongor <pongor_yuan@yeah.net>
2016-05-10 14:24:00 +0800

    a

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
-phpinfo();die;
+
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 // 定义应用目录
