68fbed96acf8e3a5b087f223b9189caa7eb9cc58
pongor <pongor_yuan@yeah.net>
2016-05-13 18:57:03 +0800

    a

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -107,7 +107,7 @@ class IndexController extends Controller {
             //下载用户头像
             $headimg = dowload($result['headimgurl'].'.jpg');
             //生成分享图片
-            $headimg = get_lt_rounder_corner(getcwd().'/'.$headimg, $result['openid']); //圆角头像
+           echo $headimg = get_lt_rounder_corner(getcwd().'/'.$headimg, $result['openid']); //圆角头像
            echo $fiel =  imgTo('./img/807893500556499641.png',$headimg,$result['nickname']);
             //上传微信素材服务器  获取素材media_id
             $file_data = array(
