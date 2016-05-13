3120bc12eb6069971e4a0901c0705c4e5d66eaf2
pongor <pongor_yuan@yeah.net>
2016-05-13 19:24:52 +0800

    a

--- App/Common/Common/function.php
+++ App/Common/Common/function.php
@@ -143,12 +143,13 @@ function imgTo($tplImg,$headImg,$codeImg,$str='pongor'){
     $image->open(getcwd().'/'.$tplImg);//->water('./img/bd_logo1.jpg',\Think\Image::IMAGE_WATER_NORTHWEST)->save(__APP__."/wechat/water.jpg");
     $head = new \Think\Image();
     $head->open(getcwd().'/'.$headImg);
-die;
+
     $image->water($headImg,\Think\Image::IMAGE_WATER_MARGIN,100,C('IMG_height')); //水印用户头像
     $height = $head->height()+C('IMG_height')+C('IMG_NAME_HEIGHT'); // 字符串据上的距离
 
     $image->text($str,'./img/ttf/msyh.ttf','20',C('IMG_TEXT_COLOR'),\Think\Image::IMAGE_WATER_MARGIN,0,0,$height);//水印用户昵称
     $code = new Think\Image();
+    die;
     $code->open(getcwd().'/'.$codeImg);
     $codeThumb = './img/temp/';
     mkDirs($codeThumb);
