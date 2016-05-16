1ee7334b37ba8c004d90c3270bb275bb6a1108eb
pongor <pongor_yuan@yeah.net>
2016-05-16 18:13:20 +0800

    a

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -63,7 +63,9 @@ class IndexController extends Controller {
                 }
 
             }
-
+            $contentStr = $postObj->Event.'-'.$id.'-'.$fromUsername;
+            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, 'text', $contentStr);
+            echo $resultStr;die;
             if( isset($res['is_start']) &&  $res['is_start'] != 1  ){
                 $contentStr = '这个活动已经结束报名啦，下次早点来哦！'.$res['is_start'].$res['id'];
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, 'text', $contentStr);
