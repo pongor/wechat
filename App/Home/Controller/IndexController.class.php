faddfea7529936c0bef4d8b1b4f109ebd1efdc54
pongor <pongor_yuan@yeah.net>
2016-05-17 11:06:11 +0800

    a

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -280,8 +280,9 @@ class IndexController extends Controller {
                          "content":"'.$a_info['re_invite_content'].'"
                     }
                 }';
-            return;
-            sendMessage($msgArray);return ;
+
+            $res = sendMessage($msgArray);
+            return ;
         }else{  //用户为支持过
             $s_data = array(
                 'user_id' => $a_user_id,
