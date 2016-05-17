5b0717584a89ae6f855f2280f7f802d5e4fc5b95
pongor <pongor_yuan@yeah.net>
2016-05-17 10:55:01 +0800

    a

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -9,7 +9,7 @@ class IndexController extends Controller {
         //OPENTM207685059
      //   $a ='<xml><ToUserName><![CDATA[gh_cbfe978fe9e3]]></ToUserName> <FromUserName><![CDATA[o0W5mswVR8UcrxhelR6e8g2eQkqA]]></FromUserName> <CreateTime>1463395333</CreateTime> <MsgType><![CDATA[event]]></MsgType> <Event><![CDATA[SCAN]]></Event> <EventKey><![CDATA[6]]></EventKey> <Ticket><![CDATA[gQFI8ToAAAAAAAAAASxodHRwOi8vd2VpeGluLnFxLmNvbS9xL25VaVdUYVBsOXVHUHNoOE0wMllWAAIE_Jw5VwMEgPQDAA==]]></Ticket> </xml>';
 
-        if(!checkSignature()){
+        if(checkSignature()){
             echo $_GET['echostr'];
 
             $xml = $GLOBALS["HTTP_RAW_POST_DATA"];
