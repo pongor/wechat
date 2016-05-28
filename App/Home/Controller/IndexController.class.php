cc59004b23c4947b2bc06d9d6f9b23414bfd3f5f
pongor <pongor_yuan@yeah.net>
2016-05-28 11:39:50 +0800

    a

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -18,13 +18,14 @@ class IndexController extends Controller {
 //    "EventKey":"11",
 //    "Ticket":"gQFN8ToAAAAAAAAAASxodHRwOi8vd2VpeGluLnFxLmNvbS9xLzdFaWxiMXZseWVHd0dXNGk0R1lWAAIEuLk6VwMEgPQDAA=="
 //}'; //扫码
+        open(json_encode($_REQUEST));
        // open(json_encode(simplexml_load_string($GLOBALS["HTTP_RAW_POST_DATA"], 'SimpleXMLElement', LIBXML_NOCDATA)));
         if(checkSignature()) {
             echo $_GET['echostr'];
             $xml = $GLOBALS["HTTP_RAW_POST_DATA"];
             $postObj = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
            //  $postObj = json_decode($json);
-              open(json_encode($xml));
+
             $fromUsername = $postObj->FromUserName;
             $toUsername = $postObj->ToUserName;
             $keyword = trim($postObj->Content);
