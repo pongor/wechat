589c3f3972a03bc901c420eb8e3273427c0bd205
pongor <pongor_yuan@yeah.net>
2016-05-28 14:35:19 +0800

    a

--- App/Common/Common/function.php
+++ App/Common/Common/function.php
@@ -23,7 +23,7 @@ function checkSignature()
     sort($tmpArr, SORT_STRING);
     $tmpStr = implode($tmpArr);
     $tmpStr = sha1($tmpStr);
-    open($tmpStr);
+    open($tmpStr.'---'.$signature);
     if( $tmpStr == $signature ){
         return true;
     }else{
