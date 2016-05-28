cab60675bfdcc45e059d253afe3f3f73afe89d6a
pongor <pongor_yuan@yeah.net>
2016-05-28 14:55:24 +0800

    a

--- App/Home/Controller/ServiceController.class.php
+++ App/Home/Controller/ServiceController.class.php
@@ -25,12 +25,12 @@ class ServiceController extends Controller
      */
     public function sendMessage($content,$openid,$type='text'){
         if(!$type) return false;
-        $msg = $this->$type($content,$openid);
+        $msg = self::$type($content,$openid);
         $result = sendMessage($msg);
         return isset($result['errcode']) && $result['errcode'] == 0;
     }
     //文本消息数据包
-    public function text($content,$openid){
+    public static function text($content,$openid){
         $msgArray = '{
                     "touser":"'.$openid.'",
                     "msgtype":"text",
